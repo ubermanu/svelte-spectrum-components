e4d18774a66581c19071dbdbba3afb52f755bca3
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 17:02:07 +0200

    feat: add close button

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import './styles.css'
 
+declare global {
+  type Size = 'S' | 'M' | 'L' | 'XL'
+}
+
 export { default as Accordion } from './Accordion.svelte'
 export { default as AccordionItem } from './AccordionItem.svelte'
 export { default as Button } from './Button.svelte'
+export { default as CloseButton } from './CloseButton.svelte'
 export { default as Form } from './Form.svelte'
 export { default as FormItem } from './FormItem.svelte'
 export { default as Icon } from './Icon.svelte'
