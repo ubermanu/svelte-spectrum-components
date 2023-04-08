c85274cd6fb9631ffe91531d7a1160caaf1219d2
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 15:49:37 +0200

    add controls

--- stories/Textfield.story.svelte
+++ stories/Textfield.story.svelte
@@ -3,10 +3,18 @@
   import { Textfield, Theme } from '../src/main.ts'
 
   export let Hst: Hst
+
+  let placeholder = 'Placeholder'
+  let invalid = false
+  let valid = false
 </script>
 
 <Hst.Story>
   <Theme>
-    <Textfield placeholder="Placeholder" value="Value" />
+    <Textfield {placeholder} {invalid} {valid} />
   </Theme>
+  <svelte:fragment slot="controls">
+    <Hst.Checkbox title="Invalid" bind:value={invalid} />
+    <Hst.Checkbox title="Valid" bind:value={valid} />
+  </svelte:fragment>
 </Hst.Story>
