7705c647af3e3a1cd1fee7f09eb846a5e68d34e3
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 18:28:14 +0200

    fix static on buttons

--- src/Button.svelte
+++ src/Button.svelte
@@ -2,21 +2,24 @@
   import '@spectrum-css/button/dist/index-vars.css'
   import '@spectrum-css/button/dist/index.css'
 
-  export let variant:
-    | 'primary'
-    | 'secondary'
-    | 'accent'
-    | 'negative'
-    | 'static' = 'accent'
+  export let variant: 'primary' | 'secondary' | 'accent' | 'negative' = 'accent'
 
   export let size: 'S' | 'M' | 'L' | 'XL' = 'M'
 
   export let treatment: 'fill' | 'outline' = 'fill'
+
+  export let staticWhite: boolean = false
+  export let staticBlack: boolean = false
+
+  const { class: additionalClasses, ...rest } = $$restProps
 </script>
 
 <button
-  class="spectrum-Button spectrum-Button--{treatment} spectrum-Button--{variant} spectrum-Button--size{size}"
-  {...$$restProps}
+  class="spectrum-Button spectrum-Button--{treatment} spectrum-Button--{variant} spectrum-Button--size{size} {additionalClasses ||
+    ''}"
+  class:spectrum-Button--staticWhite={staticWhite}
+  class:spectrum-Button--staticBlack={staticBlack}
+  {...rest}
 >
   <slot name="icon" />
   <span class="spectrum-Button-label"><slot /></span>
