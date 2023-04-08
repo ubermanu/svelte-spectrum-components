d0d87ebc5ff469657b73c3d7e23b37d9399c728f
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 18:47:42 +0200

    feat: add badge component

---
+++ src/Badge.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import type { SvelteComponent } from 'svelte'
+  import '@spectrum-css/badge/dist/index-vars.css'
+  import Icon from './Icon.svelte'
+
+  export let variant:
+    | 'neutral'
+    | 'accent'
+    | 'informative'
+    | 'positive'
+    | 'negative' = 'neutral'
+
+  export let size: TShirtSize = 'M'
+
+  export let icon: SvelteComponent | undefined = undefined
+
+  export let fixed:
+    | 'inline-start'
+    | 'inline-end'
+    | 'block-start'
+    | 'block-end'
+    | undefined = undefined
+
+  const { class: additionalClasses, ...rest } = $$restProps
+</script>
+
+<div
+  class="spectrum-Badge spectrum-Badge--size{size} spectrum-Badge--{variant} {additionalClasses ||
+    ''} {fixed ? `spectrum-Badge--fixed-${fixed}` : ''}"
+  {...rest}
+>
+  {#if icon}
+    <Icon
+      {icon}
+      class="spectrum-Badge-icon {!$$slots.default
+        ? 'spectrum-Badge-icon--no-label'
+        : ''}"
+    />
+  {/if}
+  <div class="spectrum-Badge-label">
+    <slot />
+  </div>
+</div>
---
+++ stories/Badge.story.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { Badge, Theme } from '../src/main.ts'
+  import Info18 from 'svelte-spectrum-icons/workflow/Info18.svelte'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story>
+  <Theme>
+    <Badge icon={Info18}>Neutral</Badge>
+    <Badge icon={Info18} variant="accent">Accent</Badge>
+    <Badge icon={Info18} variant="informative">Informative</Badge>
+    <Badge icon={Info18} variant="negative">Negative</Badge>
+    <Badge icon={Info18} variant="positive">Positive</Badge>
+  </Theme>
+</Hst.Story>
