4fcd169dbb007e2633b68c32e8aae139652ffc87
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 17:13:57 +0200

    feat: add action bar container

---
+++ src/ActionBar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import '@spectrum-css/actionbar/dist/index-vars.css'
+  import CloseButton from './CloseButton.svelte'
+
+  export let open: boolean = false
+  export let emphasized: boolean = false
+
+  function close() {
+    open = false
+  }
+</script>
+
+<div
+  class="spectrum-ActionBar"
+  class:spectrum-ActionBar--emphasized={emphasized}
+  class:is-open={open}
+>
+  <div class="spectrum-Popover spectrum-ActionBar-popover is-open">
+    <CloseButton on:click={close} />
+    <slot />
+  </div>
+</div>
---
+++ stories/ActionBar.story.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { ActionBar, Theme } from '../src/main.ts'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story>
+  <Theme>
+    <ActionBar open={true} />
+  </Theme>
+</Hst.Story>
