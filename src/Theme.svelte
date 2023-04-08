840cf3a26c20794549721b7a9e328eefa4e00b05
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 13:55:59 +0200

    move theme imports in component

--- src/Theme.svelte
+++ src/Theme.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+  import '@spectrum-css/vars/dist/spectrum-large.css'
+  import '@spectrum-css/vars/dist/spectrum-medium.css'
+
+  import '@spectrum-css/vars/dist/spectrum-dark.css'
+  import '@spectrum-css/vars/dist/spectrum-darkest.css'
+  import '@spectrum-css/vars/dist/spectrum-light.css'
+
   export let theme: string = 'spectrum'
   export let color: string = 'light'
   export let scale: string = 'medium'
