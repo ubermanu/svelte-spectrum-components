4ad0fe3a48c645ac869ec2413848a6afdc3d1e6a
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 17:10:57 +0200

    feat: add filed label

---
+++ src/FieldLabel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import '@spectrum-css/fieldlabel/dist/index-vars.css'
+
+  export let size: Size = 'M'
+
+  const { class: additionalClasses, ...rest } = $$restProps
+</script>
+
+<label
+  class="spectrum-FieldLabel spectrum-FieldLabel--size{size} {additionalClasses ||
+    ''}"
+  {...rest}
+>
+  <slot />
+</label>
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ declare global {
 
 export { default as Accordion } from './Accordion.svelte'
 export { default as AccordionItem } from './AccordionItem.svelte'
+export { default as ActionBar } from './ActionBar.svelte'
 export { default as Button } from './Button.svelte'
 export { default as CloseButton } from './CloseButton.svelte'
+export { default as FieldLabel } from './FieldLabel.svelte'
 export { default as Form } from './Form.svelte'
 export { default as FormItem } from './FormItem.svelte'
 export { default as Icon } from './Icon.svelte'
