8254a7dd5cbce7a197a0f2fe34d001cb8cad030a
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 19:18:20 +0200

    feat: add breadcrumb component

---
+++ src/Breadcrumb.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import '@spectrum-css/breadcrumb/dist/index-vars.css'
+
+  export let compact: boolean = false
+  export let multiline: boolean = false
+</script>
+
+<ul
+  class="spectrum-Breadcrumbs"
+  class:spectrum-Breadcrumbs--compact={compact}
+  class:spectrum-Breadcrumbs--multiline={multiline}
+>
+  <slot />
+</ul>
---
+++ stories/Breadcrumb.story.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { Breadcrumb, BreadcrumbItem, Theme } from '../src/main.ts'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story>
+  <Theme>
+    <Breadcrumb>
+      <BreadcrumbItem separator={true} label="Item 1" />
+      <BreadcrumbItem separator={true} label="Item 2" />
+      <BreadcrumbItem current={true} label="Item 3" />
+    </Breadcrumb>
+  </Theme>
+</Hst.Story>
