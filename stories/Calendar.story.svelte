50623d4c7ea454fa9eeb11e311c127eb46d189df
ubermanu <manu@paca.happy-dev.fr>
2023-04-08 19:50:19 +0200

    feat: add calendar (wip)

---
+++ stories/Calendar.story.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { Calendar, Theme } from '../src/main.ts'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story>
+  <Theme>
+    <Calendar />
+  </Theme>
+</Hst.Story>
