803655164e6cf9e08d9425fa08f9e67e3ac82861
ubermanu <manu@paca.happy-dev.fr>
2023-04-10 16:53:56 +0200

    feat: help text

---
+++ src/HelpText.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { SvelteComponent } from 'svelte'
+  import Icon from './Icon.svelte'
+
+  export let size: TShirtSize = 'M'
+  export let variant: 'neutral' | 'positive' | 'negative' = 'neutral'
+  export let icon: SvelteComponent | undefined = undefined
+  export let disabled: boolean = false
+
+  const { class: additionalClasses, ...rest } = $$restProps
+</script>
+
+<div
+  class="spectrum-HelpText spectrum-HelpText--size{size} spectrum-HelpText--{variant} {additionalClasses ||
+    ''}"
+  class:is-disabled={disabled}
+  {...rest}
+>
+  {#if icon}
+    <Icon {icon} class="spectrum-HelpText-validationIcon" />
+  {/if}
+  <div class="spectrum-HelpText-text">
+    <slot />
+  </div>
+</div>
