3a05f4167afd378a41c2173db4f4424987daff3c
ubermanu <manu@paca.happy-dev.fr>
2023-04-10 12:02:48 +0200

    feat: add asset, quick actions, card (wip)

---
+++ src/Asset.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  const { class: additionalClasses, ...rest } = $$restProps
+</script>
+
+<div class="spectrum-Asset">
+  <img class="spectrum-Asset-image {additionalClasses || ''}" alt {...rest} />
+</div>
+
+<style lang="postcss">
+  @import '@spectrum-css/asset/dist/index-vars.css';
+</style>
---
+++ src/QuickActions.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let textOnly: boolean = false
+  export let open: boolean = false
+
+  const { class: additionalClasses, ...rest } = $$restProps
+</script>
+
+<div
+  class="spectrum-QuickActions {additionalClasses || ''}"
+  class:is-open={open}
+  class:spectrum-QuickActions--textOnly={textOnly}
+  {...rest}
+>
+  <slot />
+</div>
+
+<style lang="postcss">
+  @import '@spectrum-css/quickaction/dist/index-vars.css';
+</style>
