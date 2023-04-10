cdd67e46e666936dd9d9aaaf488f3c73a3f65680
ubermanu <manu@paca.happy-dev.fr>
2023-04-10 12:29:15 +0200

    feat: button group

---
+++ src/ButtonGroup.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let size: TShirtSize = 'M'
+  export let orientation: 'horizontal' | 'vertical' = 'horizontal'
+
+  const { class: additionalClasses, ...rest } = $$restProps
+</script>
+
+<div
+  class="spectrum-ButtonGroup spectrum-ButtonGroup--size{size} {additionalClasses ||
+    ''}"
+  class:spectrum-ButtonGroup--vertical={orientation === 'vertical'}
+  {...rest}
+>
+  <slot />
+</div>
+
+<style>
+  @import '@spectrum-css/buttongroup/dist/index-vars.css';
+</style>
