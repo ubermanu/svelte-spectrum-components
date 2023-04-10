dd1837bab2ccdf708bd404b6cc9616e01b83100d
ubermanu <manu@paca.happy-dev.fr>
2023-04-10 17:54:43 +0200

    feat: add progress circle

---
+++ src/ProgressCircle.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let size: 'small' | 'medium' | 'large' = 'medium'
+  export let staticWhite: boolean = false
+  export let staticBlack: boolean = false
+  export let indeterminate: boolean = false
+
+  const { class: additionalClasses, ...rest } = $$restProps
+</script>
+
+<div
+  class="spectrum-ProgressCircle {additionalClasses || ''}"
+  class:spectrum-ProgressCircle--small={size === 'small'}
+  class:spectrum-ProgressCircle--large={size === 'large'}
+  class:spectrum-ProgressCircle--staticWhite={staticWhite}
+  class:spectrum-ProgressCircle--staticBlack={staticBlack}
+  class:spectrum-ProgressCircle--indeterminate={indeterminate}
+  {...rest}
+>
+  <div class="spectrum-ProgressCircle-track" />
+  <div class="spectrum-ProgressCircle-fills">
+    <div class="spectrum-ProgressCircle-fillMask1">
+      <div class="spectrum-ProgressCircle-fillSubMask1">
+        <div class="spectrum-ProgressCircle-fill" />
+      </div>
+    </div>
+    <div class="spectrum-ProgressCircle-fillMask2">
+      <div class="spectrum-ProgressCircle-fillSubMask2">
+        <div class="spectrum-ProgressCircle-fill" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  @import '@spectrum-css/progresscircle/dist/index-vars.css';
+</style>
---
+++ stories/ProgressCircle.story.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { ProgressCircle, Theme } from '../src/main.ts'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story>
+  <Theme>
+    <div class="list">
+      <ProgressCircle size="small" />
+      <ProgressCircle />
+      <ProgressCircle size="large" />
+      <ProgressCircle indeterminate />
+    </div>
+  </Theme>
+</Hst.Story>
+
+<style>
+  .list {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: 1rem;
+  }
+</style>
