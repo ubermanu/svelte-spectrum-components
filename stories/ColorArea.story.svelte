f53fa614267fd0205d0c77a82158890d80ac47db
ubermanu <manu@paca.happy-dev.fr>
2023-04-10 12:50:16 +0200

    add color area (wip)

---
+++ stories/ColorArea.story.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+
+  import { ColorArea, ColorHandle } from '../src/main'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story>
+  <ColorArea color="#ff00ff" />
+  <ColorHandle color="#ff00ff" />
+</Hst.Story>
