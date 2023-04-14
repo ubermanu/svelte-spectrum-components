a2b110954b81698b4b63fb2f1d6b1027cadb83d4
ubermanu <manu@paca.happy-dev.fr>
2023-04-14 13:51:47 +0200

    feat: tooltip

---
+++ src/Tooltip/Tooltip.story.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import { Tooltip, Theme } from '..'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story>
+  <Theme>
+    <Tooltip
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing eli"
+      open
+    />
+  </Theme>
+</Hst.Story>
