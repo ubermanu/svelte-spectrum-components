ec29b85ca4b70b1b6bf657bf42ffac6f33ab977d
ubermanu <manu@paca.happy-dev.fr>
2023-04-18 16:28:40 +0200

    chore: update accordion story

--- src/lib/components/Accordion/Accordion.story.svelte
+++ src/lib/components/Accordion/Accordion.story.svelte
@@ -5,42 +5,45 @@
   export let Hst: Hst
 </script>
 
-<Hst.Story>
-  <Theme>
-    <p>Default</p>
-    <Accordion>
-      <AccordionItem label="Recent" open>
-        <p>Item 1</p>
-      </AccordionItem>
-      <AccordionItem label="Architecture" disabled>
-        <p>Item 2</p>
-      </AccordionItem>
-      <AccordionItem label="Nature">
-        <p>Item 3</p>
-      </AccordionItem>
-      <AccordionItem
-        label="Really Long Accordion Item According to Our Predictions"
-      >
-        <p>Item 4</p>
-      </AccordionItem>
-    </Accordion>
-    <br />
-    <p>Multiple</p>
-    <Accordion allowMultiple>
-      <AccordionItem label="Recent" open>
-        <p>Item 1</p>
-      </AccordionItem>
-      <AccordionItem label="Architecture" disabled>
-        <p>Item 2</p>
-      </AccordionItem>
-      <AccordionItem label="Nature">
-        <p>Item 3</p>
-      </AccordionItem>
-      <AccordionItem
-        label="Really Long Accordion Item According to Our Predictions"
-      >
-        <p>Item 4</p>
-      </AccordionItem>
-    </Accordion>
-  </Theme>
+<Hst.Story layout={{ type: 'grid', width: '100%' }}>
+  <Hst.Variant title="Standard">
+    <Theme>
+      <Accordion>
+        <AccordionItem label="Recent" open>
+          <p>Item 1</p>
+        </AccordionItem>
+        <AccordionItem label="Architecture" disabled>
+          <p>Item 2</p>
+        </AccordionItem>
+        <AccordionItem label="Nature">
+          <p>Item 3</p>
+        </AccordionItem>
+        <AccordionItem
+          label="Really Long Accordion Item According to Our Predictions"
+        >
+          <p>Item 4</p>
+        </AccordionItem>
+      </Accordion>
+    </Theme>
+  </Hst.Variant>
+  <Hst.Variant title="Multiple">
+    <Theme>
+      <Accordion allowMultiple>
+        <AccordionItem label="Recent" open>
+          <p>Item 1</p>
+        </AccordionItem>
+        <AccordionItem label="Architecture" disabled>
+          <p>Item 2</p>
+        </AccordionItem>
+        <AccordionItem label="Nature">
+          <p>Item 3</p>
+        </AccordionItem>
+        <AccordionItem
+          label="Really Long Accordion Item According to Our Predictions"
+        >
+          <p>Item 4</p>
+        </AccordionItem>
+      </Accordion>
+    </Theme>
+  </Hst.Variant>
 </Hst.Story>
