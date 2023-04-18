dd7f67ea3b67a61cef455af85b75c3099b44184d
ubermanu <manu@paca.happy-dev.fr>
2023-04-18 12:13:57 +0200

    chore: pass class to inner theme div

--- src/lib/components/Theme/Theme.svelte
+++ src/lib/components/Theme/Theme.svelte
@@ -2,8 +2,13 @@
   export let theme: string = 'spectrum'
   export let color: string = 'light'
   export let scale: string = 'medium'
+
+  const { class: additionalClasses = '', ...rest } = $$restProps
 </script>
 
-<div class="spectrum spectrum--{color} spectrum--{scale}" {...$$restProps}>
+<div
+  class="spectrum spectrum--{color} spectrum--{scale} {additionalClasses}"
+  {...rest}
+>
   <slot />
 </div>
