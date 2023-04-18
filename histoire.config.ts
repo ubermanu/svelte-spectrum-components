6ff3cadfad66b5cc322a48d46c42ba50e769ae38
ubermanu <manu@paca.happy-dev.fr>
2023-04-18 16:18:49 +0200

    feat: publish storybook to gh pages

--- histoire.config.ts
+++ histoire.config.ts
@@ -2,5 +2,8 @@ import { HstSvelte } from '@histoire/plugin-svelte'
 import { defineConfig } from 'histoire'
 
 export default defineConfig({
+  vite: {
+    base: '/svelte-spectrum-components/',
+  },
   plugins: [HstSvelte()],
 })
