769086125aba913773a97219de4b4f4f2c5d27cc
ubermanu <manu@paca.happy-dev.fr>
2023-04-21 17:06:24 +0200

    feat: split button

---
+++ src/lib/components/SplitButton/SplitButton.story.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import SplitButton from './SplitButton.svelte'
+  import Theme from '$lib/histoire/Theme.svelte'
+  import { logEvent } from 'histoire/client'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story layout={{ type: 'grid', width: '100%' }}>
+  <Hst.Variant title="CTA">
+    <Theme>
+      <SplitButton
+        variant="accent"
+        on:click={(e) => logEvent('click', e)}
+        on:trigger={(e) => logEvent('trigger', e)}
+      >
+        Split Button
+      </SplitButton>
+      <br />
+      <SplitButton
+        variant="accent"
+        placement="left"
+        on:click={(e) => logEvent('click', e)}
+        on:trigger={(e) => logEvent('trigger', e)}
+      >
+        Split Button
+      </SplitButton>
+    </Theme>
+  </Hst.Variant>
+  <Hst.Variant title="Primary">
+    <Theme>
+      <SplitButton
+        variant="primary"
+        treatment="outline"
+        on:click={(e) => logEvent('click', e)}
+        on:trigger={(e) => logEvent('trigger', e)}
+      >
+        Split Button
+      </SplitButton>
+      <br />
+      <SplitButton
+        variant="primary"
+        treatment="outline"
+        placement="left"
+        on:click={(e) => logEvent('click', e)}
+        on:trigger={(e) => logEvent('trigger', e)}
+      >
+        Split Button
+      </SplitButton>
+    </Theme>
+  </Hst.Variant>
+  <Hst.Variant title="Secondary">
+    <Theme>
+      <SplitButton
+        variant="secondary"
+        treatment="outline"
+        on:click={(e) => logEvent('click', e)}
+        on:trigger={(e) => logEvent('trigger', e)}
+      >
+        Split Button
+      </SplitButton>
+      <br />
+      <SplitButton
+        variant="secondary"
+        treatment="outline"
+        placement="left"
+        on:click={(e) => logEvent('click', e)}
+        on:trigger={(e) => logEvent('trigger', e)}
+      >
+        Split Button
+      </SplitButton>
+    </Theme>
+  </Hst.Variant>
+</Hst.Story>
---
+++ src/lib/components/SplitButton/SplitButton.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import type { TShirtSize } from '$lib/spectrum/types'
+  import { Button, Icon } from '$lib'
+  import { ChevronRightSmall as Chevron100 } from 'svelte-spectrum-icons/ui'
+  import { createEventDispatcher } from 'svelte'
+
+  export let size: TShirtSize = 'M'
+  export let variant: 'primary' | 'secondary' | 'accent' = 'primary'
+  export let placement: 'left' | 'right' = 'right'
+  export let treatment: 'fill' | 'outline' = 'fill'
+
+  const dispatch = createEventDispatcher()
+</script>
+
+<div
+  class="spectrum-SplitButton"
+  class:spectrum-SplitButton--left={placement === 'left'}
+>
+  {#if placement === 'left'}
+    <Button
+      {size}
+      {variant}
+      {treatment}
+      class="spectrum-SplitButton-trigger"
+      on:click={() => dispatch('trigger')}
+    >
+      <Icon
+        slot="icon"
+        icon={Chevron100}
+        class="spectrum-UIIcon-ChevronDown100 spectrum-SplitButton-icon"
+      />
+    </Button>
+  {/if}
+  <Button
+    {size}
+    {variant}
+    {treatment}
+    class="spectrum-SplitButton-action"
+    on:click
+  >
+    Split Button
+  </Button>
+  {#if placement === 'right'}
+    <Button
+      {size}
+      {variant}
+      {treatment}
+      class="spectrum-SplitButton-trigger"
+      on:click={() => dispatch('trigger')}
+    >
+      <Icon
+        slot="icon"
+        icon={Chevron100}
+        class="spectrum-UIIcon-ChevronDown100 spectrum-SplitButton-icon"
+      />
+    </Button>
+  {/if}
+</div>
