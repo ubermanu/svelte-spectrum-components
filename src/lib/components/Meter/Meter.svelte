590810f5fb32e522d9a660ca5bffccc301335266
ubermanu <manu@paca.happy-dev.fr>
2023-04-21 19:22:28 +0200

    feat: meter

---
+++ src/lib/components/Meter/Meter.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { ProgressBar } from '$lib'
+
+  export let size: 'S' | 'L' = 'L'
+  export let variant: 'positive' | 'notice' | 'negative' | null = null
+
+  const variantClassMap = {
+    positive: 'is-positive',
+    notice: 'is-notice',
+    negative: 'is-negative',
+  }
+
+  // FIXME: Make the progressbar ignore the size prop when it's null
+
+  const variantClass = variantClassMap[variant] ?? ''
+  const { class: additionalClasses = '', ...rest } = $$restProps
+</script>
+
+<ProgressBar
+  class="spectrum-Meter--size{size} {variantClass} {additionalClasses}"
+  size={null}
+  {...rest}
+/>
