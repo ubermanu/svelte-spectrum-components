a6a5b34a7ac0d3318c34ff6639182e15c37a7bb0
ubermanu <manu@paca.happy-dev.fr>
2023-04-21 17:27:27 +0200

    feat: radio button

---
+++ src/lib/components/Radio/Radio.story.svelte
@@ -0,0 +1,168 @@
+<script lang="ts">
+  import type { Hst } from '@histoire/plugin-svelte'
+  import Radio from './Radio.svelte'
+  import Theme from '$lib/histoire/Theme.svelte'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story layout={{ type: 'grid', width: '100%' }}>
+  <Hst.Variant title="Sizing">
+    <Theme>
+      <div class="spectrum-Examples">
+        <div class="spectrum-Examples-item">
+          <form>
+            <Radio name="pets" label="Kittens" size="S" checked />
+            <br />
+            <Radio name="pets" label="Puppies" size="S" />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <form>
+            <Radio name="pets" label="Kittens" size="M" checked />
+            <br />
+            <Radio name="pets" label="Puppies" size="M" />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <form>
+            <Radio name="pets" label="Kittens" size="L" checked />
+            <br />
+            <Radio name="pets" label="Puppies" size="L" />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <form>
+            <Radio name="pets" label="Kittens" size="XL" checked />
+            <br />
+            <Radio name="pets" label="Puppies" size="XL" />
+          </form>
+        </div>
+      </div>
+    </Theme>
+  </Hst.Variant>
+
+  <Hst.Variant title="Standard">
+    <Theme>
+      <div class="spectrum-Examples">
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Default
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" checked />
+            <br />
+            <Radio name="pets" label="Puppies" />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Invalid
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" checked invalid />
+            <br />
+            <Radio name="pets" label="Puppies" invalid />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Disabled
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" checked disabled />
+            <br />
+            <Radio name="pets" label="Puppies" disabled />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Read-only
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" checked readonly />
+            <br />
+            <Radio name="pets" label="Puppies" readonly />
+          </form>
+        </div>
+      </div>
+    </Theme>
+  </Hst.Variant>
+
+  <Hst.Variant title="Emphasized">
+    <Theme>
+      <div class="spectrum-Examples">
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Default
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" emphasized checked />
+            <br />
+            <Radio name="pets" label="Puppies" emphasized />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Invalid
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" emphasized checked invalid />
+            <br />
+            <Radio name="pets" label="Puppies" emphasized invalid />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Disabled
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" emphasized checked disabled />
+            <br />
+            <Radio name="pets" label="Puppies" emphasized disabled />
+          </form>
+        </div>
+        <div class="spectrum-Examples-item">
+          <h4
+            class="spectrum-Heading spectrum-Heading--sizeXS spectrum-Examples-itemHeading"
+          >
+            Read-only
+          </h4>
+          <form>
+            <Radio name="pets" label="Kittens" emphasized checked readonly />
+            <br />
+            <Radio name="pets" label="Puppies" emphasized readonly />
+          </form>
+        </div>
+      </div>
+    </Theme>
+  </Hst.Variant>
+
+  <Hst.Variant title="Wrapping behavior">
+    <Theme>
+      <Radio>
+        Radio with an extraordinarily long label please don't do this but if you
+        did it should wrap text when it gets longer than the container which
+        contains the radio which has an unacceptably long label
+      </Radio>
+    </Theme>
+  </Hst.Variant>
+</Hst.Story>
+
+<style global>
+  @import '$lib/histoire/styles.css';
+</style>
---
+++ src/lib/components/Radio/Radio.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { v4 as uuid } from '@lukeed/uuid'
+  import type { TShirtSize } from '$lib/spectrum/types'
+
+  export let size: TShirtSize = 'M'
+  export let emphasized: boolean = false
+
+  export let id = uuid()
+  export let label: string = ''
+  export let readonly: boolean = false
+  export let invalid: boolean = false
+  export let disabled: boolean = false
+
+  const { class: additionalClasses = '', ...rest } = $$restProps
+</script>
+
+<div
+  class="spectrum-Radio spectrum-Radio--size{size}"
+  class:spectrum-Radio--emphasized={emphasized}
+  class:is-readOnly={readonly}
+  class:is-invalid={invalid}
+  class:is-disabled={disabled}
+>
+  <input
+    type="radio"
+    class="spectrum-Radio-input {additionalClasses}"
+    {id}
+    {disabled}
+    {readonly}
+    {invalid}
+    {...rest}
+  />
+  <span class="spectrum-Radio-button spectrum-Radio-button--size{size}" />
+  <label class="spectrum-Radio-label spectrum-Radio-label--size{size}" for={id}>
+    {#if $$slots.default}
+      <slot />
+    {:else}
+      {label}
+    {/if}
+  </label>
+</div>
